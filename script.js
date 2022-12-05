c555007521b05a86f6fc77a1ab789b1198b547f4
Denis <denischen242@gmail.com>
2022-12-05 14:27:17 -0500

    woahh

--- script.js
+++ script.js
@@ -13,13 +13,76 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     d1.ch_display();
-    canvas.addEventListener("keydown", controls);
+    document.addEventListener('keydown', controls);
+    document.addEventListener('keyup', controls);
+    console.log('hello')
+
 
 }
 
-function controls(key) {
-    console.log('upu ')
+function controls(e) {
+    if (e.key.toLowerCase() == 'w') {
+        d1.py -= 4.0;
+    }
 }
+
+// movement code (need to make it smooth (i.e. pressing w and a moves you up and left together))
+var velY = 0,
+    velX = 0,
+    friction = 0.78, // friction
+    keys = [];
+
+function update() {
+    requestAnimationFrame(update);
+
+    // check the keys and do the movement.
+    if (keys['w']) {
+        if (velY > -d1.stat.vel) {
+            velY--;
+        }
+    }
+
+    if (keys['s']) {
+        if (velY < d1.stat.vel) {
+            velY++;
+        }
+    }
+    if (keys['d']) {
+        if (velX < d1.stat.vel) {
+            velX++;
+        }
+    }
+    if (keys['a']) {
+        if (velX > -d1.stat.vel) {
+            velX--;
+        }
+    }
+
+    // apply some friction to y velocity.
+    velY *= friction;
+    d1.py += velY;
+
+    // apply some friction to x velocity.
+    velX *= friction;
+    d1.px += velX;
+
+    // bounds checking
+    if (d1.px >= WIDTH-5) {
+        d1.px = WIDTH-5;
+    } else if (d1.px <= 5) {
+        d1.px = 5;
+    }
+
+    if (d1.py > WIDTH-5) {
+        d1.py = WIDTH-5;
+    } else if (d1.py <= 5) {
+        d1.py = 5;
+    }
+}
+
+
+
+
 
 
 class Entity {
@@ -38,7 +101,8 @@ class Being extends Entity {
             hp: 1000,
             max_hp: 1000,
             atk: 50,
-            alive: true
+            alive: true,
+            vel: 5
         }
         this.rtx = {
             'fill':'rgb(0,0,0)'
@@ -136,3 +200,20 @@ function sungodgraphics(divine) {
 
 setup();
 setInterval(draw, 16);
+update();
+
+// key events
+document.body.addEventListener("keydown", function (e) {
+    keys[e.key] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+    keys[e.key] = false;
+});
+
+
+
+
+
+
+
+// smooth movement https://stackoverflow.com/questions/15344104/smooth-character-movement-in-canvas-game-using-keyboard-controls
